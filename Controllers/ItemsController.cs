d1475733371a62cb5a17f526b2afe086b6ab2988
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-03-20 05:58:28 +0700

    feat (auth): implement auth controller

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -4,37 +4,35 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
+using InventoryItems.Helper;
 
 namespace InventoryItems.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-    public class ItemsController : ControllerBase
+    public class ItemsController(ApplicationDbContext context) : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
-        public ItemsController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+        private readonly ApplicationDbContext _context = context;
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Item>>> GetItems()
+        public async Task<ActionResult<ApiResponse>> GetItems()
         {
             if (!User.Identity.IsAuthenticated)
             {
-                return HandleUnauthorized();
+                return ApiResponse.HandleUnauthorized();
             }
 
-            return await _context.Items.ToListAsync();
+            var items = await _context.Items.ToListAsync();
+            return ApiResponse.SuccessResponse(items);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Item>> GetItem(int id)
+        public async Task<ActionResult<ApiResponse>> GetItem(int id)
         {
             if (!User.Identity.IsAuthenticated)
             {
-                return HandleUnauthorized();
+                return ApiResponse.HandleUnauthorized();
             }
 
             var item = await _context.Items.FindAsync(id);
@@ -44,15 +42,15 @@ namespace InventoryItems.Controllers
                 return NotFound();
             }
 
-            return Ok(item);
+            return ApiResponse.SuccessResponse(item);
         }
 
         [HttpPost]
-        public async Task<ActionResult<Item>> PostItem(Item itemData)
+        public async Task<ActionResult<ApiResponse>> PostItem(Item itemData)
         {
             if (!User.Identity.IsAuthenticated)
             {
-                return HandleUnauthorized();
+                return ApiResponse.HandleUnauthorized();
             }
 
             _context.Items.Add(itemData);
@@ -61,11 +59,11 @@ namespace InventoryItems.Controllers
         }
 
         [HttpPut("{id}")]
-        public async Task<ActionResult> PutItem(int id, Item itemData)
+        public async Task<ActionResult<ApiResponse>> PutItem(int id, Item itemData)
         {
             if (!User.Identity.IsAuthenticated)
             {
-                return HandleUnauthorized();
+                return ApiResponse.HandleUnauthorized();
             }
 
             if (id != itemData.InventoryID)
@@ -94,11 +92,11 @@ namespace InventoryItems.Controllers
         }
 
         [HttpDelete("{id}")]
-        public async Task<ActionResult> DeleteItem (int id)
+        public async Task<ActionResult<ApiResponse>> DeleteItem (int id)
         {
             if (!User.Identity.IsAuthenticated)
             {
-                return HandleUnauthorized();
+                return ApiResponse.HandleUnauthorized();
             }
 
             var item = await _context.Items.FindAsync(id);
@@ -115,11 +113,6 @@ namespace InventoryItems.Controllers
         private bool ItemExists(int id)
         {
             return _context.Items.Any(e => e.InventoryID == id);
-        }
-
-        private ActionResult<IEnumerable<Item>> HandleUnauthorized()
-        {
-            return StatusCode(401, "Unauthorized access. Please provide a valid access token.");
         }
     }
 }
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -4,119 +4,40 @@ using InventoryItems.Data;
 using InventoryItems.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using InventoryItems.Dto;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using InventoryItems.Helper;
+
 
 namespace InventoryItems.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class UsersController(ApplicationDbContext context, IConfiguration configuration) : ControllerBase
     {
         private readonly ApplicationDbContext _context = context;
-        private readonly SymmetricSecurityKey _jwtSecretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:SecretKey"]));
-        private readonly string _jwtIssuer = configuration["JwtSettings:Issuer"];
-        private readonly string _jwtAudience = configuration["JwtSettings:Audience"];
-
-        [HttpPost("Login")]
-        public async Task<ActionResult<User>> Login(UserLoginDto userLogin)
-        {
-            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == userLogin.Username && u.Password == userLogin.Password);
-            if (user == null)
-            {
-                return Unauthorized();
-            }
-
-            var token = GenerateJwtToken(user);
-            user.AccessToken = token;
-
-            _context.Users.Update(user);
-            await _context.SaveChangesAsync();
-
-            var response = new
-            {
-                data = new
-                {
-                    accessToken = token,
-                    createdAt = user.CreatedAt,
-                    updatedAt = user.UpdateAt
-                }
-            };
-
-            return Ok(response);
-        }
-
-        [HttpPost("Logout")]
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
-        public async Task<IActionResult> Logout()
-        {
-            var accessToken = Request.Headers["Authorization"].FirstOrDefault()?.Split(' ').Last();
-            if (string.IsNullOrEmpty(accessToken))
-            {
-                return BadRequest("Access token is missing.");
-            }
-
-            var handler = new JwtSecurityTokenHandler();
-            var token = handler.ReadToken(accessToken) as JwtSecurityToken;
-            if (token == null)
-            {
-                return BadRequest("Invalid access token.");
-            }
-
-            var userIdClaim = token.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
-            if (userIdClaim == null || !int.TryParse(userIdClaim.Value, out int userId))
-            {
-                return BadRequest("Invalid user information.");
-            }
-
-            var user = await _context.Users.FindAsync(userId);
-            if (user == null)
-            {
-                return NotFound("User not found.");
-            }
-
-            user.AccessToken = null;
-            _context.Users.Update(user);
-            await _context.SaveChangesAsync();
-
-            return Ok("Logged out successfully.");
-        }
-
-        private string GenerateJwtToken(User user)
-        {
-            var claims = new[]
-            {
-                new Claim(ClaimTypes.NameIdentifier, user.UserID.ToString()),
-                new Claim(ClaimTypes.Name, user.Username),
-                new Claim(ClaimTypes.Email, user.Email),
-            };
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var tokenDescriptor = new SecurityTokenDescriptor
-            {
-                Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddDays(7),
-                Issuer = _jwtIssuer,
-                Audience = _jwtAudience,
-                SigningCredentials = new SigningCredentials(_jwtSecretKey, SecurityAlgorithms.HmacSha256Signature)
-            };
-
-            var token = tokenHandler.CreateToken(tokenDescriptor);
-            return tokenHandler.WriteToken(token);
-        }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        public async Task<ActionResult<ApiResponse>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            if (!User.Identity.IsAuthenticated)
+            {
+                return ApiResponse.HandleUnauthorized();
+            }
+
+            var users = await _context.Users.ToListAsync();
+            return ApiResponse.SuccessResponse(users);
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<User>> GetUser(int id)
+        public async Task<ActionResult<ApiResponse>> GetUser(int id)
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return ApiResponse.HandleUnauthorized();
+            }
+
             var user = await _context.Users.FindAsync(id);
 
             if (user == null)
@@ -124,12 +45,26 @@ namespace InventoryItems.Controllers
                 return NotFound();
             }
 
-            return user;
+            return ApiResponse.SuccessResponse(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserID }, user);
         }
 
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutUser(int id, User user)
+        public async Task<ActionResult<ApiResponse>> PutUser(int id, User user)
         {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return ApiResponse.HandleUnauthorized();
+            }
+
             if (id != user.UserID)
             {
                 return BadRequest();
@@ -154,15 +89,6 @@ namespace InventoryItems.Controllers
             }
 
             return NoContent();
-        }
-
-        [HttpPost]
-        public async Task<ActionResult<User>> PostUser(User user)
-        {
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetUser", new { id = user.UserID }, user);
         }
 
         [HttpDelete("{id}")]
